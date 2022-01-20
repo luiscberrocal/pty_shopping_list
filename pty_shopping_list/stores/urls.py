b4f3e5c135674a04e5baa0ac3f1fdf85d52821c5
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2022-01-20 14:13:41 -0500

    Half baked solution

--- pty_shopping_list/stores/urls.py
+++ pty_shopping_list/stores/urls.py
@@ -5,5 +5,11 @@ from pty_shopping_list.stores.views import store_chain_create_view
 app_name = 'stores'
 
 urlpatterns = [
-    path(r'chain/create', store_chain_create_view, name='create-chain-store')
+    #ChainStore CRUD urls
+    path(r'chain-store/list/', chain_store_list_view, name='list-chain-store'),
+    path(r'chain-store/create/', chain_store_create_view, name='create-chain-store'),
+    path(r'chain-store/update/<int:pk>/', chain_store_update_view, name='update-chain-store'),
+    path(r'chain-store/delete/<int:pk>/', chain_store_delete_view, name='delete-chain-store'),
+    path(r'chain-store/<int:pk>/', chain_store_detail_view, name='detail-chain-store'),
+
 ]
--- pty_shopping_list/stores/views.py
+++ pty_shopping_list/stores/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 # Create your views here.
 from django.views.generic import CreateView
 
@@ -14,3 +13,36 @@ class StoreChainCreateView(CreateView):
 
 
 store_chain_create_view = StoreChainCreateView.as_view()
+
+
+class StoreChainUpdateView(LoginRequiredMixin, UpdateView):
+    model = StoreChain
+    form_class = StoreChainForm
+    success_url = reverse_lazy('stores:list-store-chain')
+
+
+store_chain_update_view = StoreChainUpdateView.as_view()
+
+
+class StoreChainListView(LoginRequiredMixin, ListView):
+    model = StoreChain
+    context_object_name = 'store_chain_list'
+    paginate_by = 10
+
+
+store_chain_list_view = StoreChainListView.as_view()
+
+
+class StoreChainDeleteView(LoginRequiredMixin, DeleteView):
+    model = StoreChain
+    success_url = reverse_lazy('stores:list-store-chain')
+
+
+store_chain_delete_view = StoreChainDeleteView.as_view()
+
+
+class StoreChainDetailView(LoginRequiredMixin, DetailView):
+    model = StoreChain
+
+
+store_chain_detail_view = StoreChainDetailView.as_view()
