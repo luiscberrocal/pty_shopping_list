bffc3de4b726f5b03ccc3b3af22899de19bc5b74
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2022-01-26 07:54:19 -0500

    Saving metadata. WIP.

--- pty_shopping_list/stores/forms.py
+++ pty_shopping_list/stores/forms.py
@@ -1,10 +1,29 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from pty_shopping_list.stores.models import StoreChain
 
 
 class StoreChainForm(forms.ModelForm):
+    SUPER_MARKET_TYPE = 'SUPER_MARKET'
+    DRUG_STORE_TYPE = 'DRUG_STORE'
+    OTHER_TYPE = 'OTHER'
+    TYPE_CHOICES = (
+        (SUPER_MARKET_TYPE, _('Supermarket')),
+        (DRUG_STORE_TYPE, _('Drug store')),
+        (OTHER_TYPE, _('Other')),
+    )
+    chain_type = forms.ChoiceField(label=_('Chain type'), choices=TYPE_CHOICES,)
+
     class Meta:
         model = StoreChain
-        fields = ('id', 'name', 'closed_on')
-        widgets = {'closed_on': forms.DateInput(attrs={'type': 'date'})}
+        fields = ('id', 'name', 'closed_on', 'metadata')
+        widgets = {'closed_on': forms.DateInput(attrs={'type': 'date'}),
+                   'metadata': forms.HiddenInput()}
+
+    def clean(self):
+        cleaned_data =super(StoreChainForm, self).clean()
+        cleaned_data['metatadata'] = {'chain_type': cleaned_data['chain_type']}
+        return cleaned_data
+
+    #def save(self, force_insert=False, force_update=False):
